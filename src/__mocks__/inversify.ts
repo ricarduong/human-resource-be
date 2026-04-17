b7927ea7cd9f07db08ff36360daa12beae4dc1d4
Nguyen Thai Duong <nduong@Nguyens-MacBook-Air.local>
2026-04-18 01:08:40 +0700

    fix eslint

--- src/__mocks__/inversify.ts
+++ src/__mocks__/inversify.ts
@@ -1,4 +1,5 @@
-export const injectable = () => (_target: unknown) => _target;
+export const injectable = () => (target: unknown) => target;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const inject = (_id: unknown) => (_target: unknown, _key: unknown, _index: unknown) => {};
 export const Container = jest.fn();
 export const LazyServiceIdentifier = jest.fn();
